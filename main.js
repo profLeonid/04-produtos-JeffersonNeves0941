1b46ced1ea5138f6a66834da5ffe2d9bfb936926
JEFFERSON VICTOR NEVES DA SILVA <25204074@dsn127m1246728.cfp127aluno.local>
2026-02-24 16:59:49 -0300

    criando mais 2 caixa input

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 'use strict'
 function adicionarProduto(){
     const produto = document.getElementById("produto")
+      const codigo = document.getElementById("codigo")
+      const quantidade = document.getElementById("quantidade")
+
     const lista = document.getElementById("lista")
 
-    const span = document.createElement("span") 
+    const codspan = document.createElement("codigo") 
+    const prodspan`` = document.createElement("codigo")
     span.textContent = produto.value
     span.className = "bg-blue-200 px-8 py-2"
  
     lista.appendChild(span)
 
+    if(produto == ""){
+    return false
+    } 
 }
+
